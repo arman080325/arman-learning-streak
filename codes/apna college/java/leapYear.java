16a31c21f4e04971c6c84aa05d408cce219212bb
arman080325 <armankhan082020@gmail.com>
2026-07-14 23:56:02 +0530

    log: 2026-07-14

--- codes/apna college/java/leapYear.java
+++ codes/apna college/java/leapYear.java
@@ -8,7 +8,7 @@ public class leapYear {
         System.out.println("Enter a year to check if it is a leap year or not: ");
         Scanner sc = new Scanner(System.in);
         year = sc.nextInt();
-        if ((year % 4 == 0 && year % 100 == 0) || year % 400 == 0) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             System.out.println("Year " + year + " is a leap year.");
         } else {
             System.out.println("Year " + year + " is not a leap year.");
